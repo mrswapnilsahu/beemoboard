f20f30fe8abdb50c78f868a08f85a025195c13e1
Brandon Foltz <bsfoltzguitar@gmail.com>
2012-03-28 01:45:56 -0400

    Figured out the git pulling bit, should work now.

--- gitpull.php
+++ gitpull.php
@@ -2,11 +2,5 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-/*if ($_SERVER['REMOTE_ADDR'] == "207.97.227.253" ||
-	 $_SERVER['REMOTE_ADDR'] == "50.57.128.197")
-{*/
-	exec("git pull", $output);
-	echo "<pre>";
-	print_r($output);
-//}
+echo shell_exec("git pull");
 ?>
